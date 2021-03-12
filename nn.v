f252d56e43a8ccdb997f20c96e7564de0b479006
idatewang <43754687+idatewang@users.noreply.github.com>
2021-03-12 02:29:57 -0600

    tested functions, main() and print for nn.v

--- nn.v
+++ nn.v
@@ -1,12 +1,41 @@
-`timescale 1ns / 1ps module 
-full_adder( A, B, Cin, S, Cout);
+module main;
+ 	// global vars
+ 	reg [7:0] a [0:3];
+ 	reg [7:0] b [0:3] [0:3];
+ 	reg [7:0] i,j;
+ 	reg [8*14:1] string_test = "ad";
+ 	// functions
+	// function automatic [7:0] factorial;
+ //    	input signed [7:0] i_Num; 
+ //    	begin
+ //      		if (i_Num == 1)
+ //        		factorial = 1; 
+ //      		else
+ //        		factorial = i_Num * factorial(i_Num-1);
+ //    	end
+ //  	endfunction
 
- input wire A, B, Cin;
- output reg S, Cout;
+	// function automatic [7:0] sigmoid_deriv;
+ //  		input signed [7:0] x;
+ //  		begin
+ //  			sigmoid_deriv = x * (1-x);
+ //  		end
+ //  	endfunction
 
- always @(A or B or Cin)
-  begin 
-   S = A ^ B ^ Cin; 
-   Cout = A&B | (A^B) & Cin; 
-  end
+
+  	// main()
+	initial begin
+		for (i=0; i<=3; i++) begin
+      		for (j=0; j<=3; j++) begin
+          		b[i][j] = i*j;
+          	end
+        end
+
+        for (i=0; i<=3; i++) begin
+      		for (j=0; j<=3; j++) begin
+          		$write(b[i][j]);
+          	end
+          	$display();
+        end
+    end
 endmodule
