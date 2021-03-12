7c6952af68f55e498137e1e1a9250da2045b64f8
idatewang <43754687+idatewang@users.noreply.github.com>
2021-03-12 00:46:39 -0600

    Verilog test, import python code from repo

---
+++ nn.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps module 
+full_adder( A, B, Cin, S, Cout);
+
+ input wire A, B, Cin;
+ output reg S, Cout;
+
+ always @(A or B or Cin)
+  begin 
+   S = A ^ B ^ Cin; 
+   Cout = A&B | (A^B) & Cin; 
+  end
+endmodule
